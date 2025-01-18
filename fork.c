03a90c87ac2418fd2b2c226263f62a2bd14d66dc
HansiUpekka <hansiupekkasithumini2002@gmail.com>
2025-01-18 22:07:57 +0530

    final edition

--- fork.c
+++ fork.c
@@ -1,46 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> // for fork() and exit() functions
+#include <unistd.h> // for fork(), exit() and sleep()  functions
 #include <sys/types.h>  // for pid_t type
-
+#include <sys/wait.h> // for wait()
 
 int main()
 {
-    pid_t pid1, pid2;
+    pid_t child1, child2;
+    
+    printf("PARENT: PID = %d\n",getpid()); // parentprocess prints its PID
 
-    pid1 = fork(); // create a child process
+    child1 = fork(); // create a child process
 
-    if (pid1 < 0)
+    if (child1 < 0)
     {
         printf("Fork Failed to create first child.\n");
         exit(1);
     }
 
-    else if (pid1 == 0)
+    else if (child1 == 0)
     {
         printf ("Child 1\n");
+	printf("CHILD 1 : PID = %d, PPID = %d\n\n",getpid(), getppid());
         fflush(stdout); //ensure output is flushed immediately
+	sleep(20);      // keep the process running for verification
+	exit(0);        //exit child process
     }
     else
     {
-        pid2 = fork(); // create another child process
+        child2 = fork(); // create another child process
 
-        if (pid2 < 0)
+        if (child2 < 0)
         {
             printf("Fork Failed to create second child.\n");
             exit(1);
         }
-        else if (pid2 == 0)
+        else if (child2 == 0)
         {
             printf ("Child 2\n");
+	    printf("CHILD 2 : PID = %d, PPID = %d\n\n",getpid(),getppid());
             fflush(stdout); //ensure output is flushed immediately
+	    sleep(20);      //keep the process running for verification
+	    exit(0);        // exit child process	
         }
         else
         {
-            wait(NULL); // wait for child to finish
-            wait(NULL); // wait for child to finish
-            printf ("Parent\n");
+	    // parent process
+	    wait(NULL); //wait for the first child to finish
+	    wait(NULL); // wait for the second child to finish
+            printf ("Parent : PID = %d\n",getpid());
             fflush(stdout); //ensure output is flushed immediately
+	    sleep(10); //keep the parent process running for verification
         }
     }
 }
